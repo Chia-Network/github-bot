d69a97f61fe124e3918c8479349a320d204775f1
Patrick Maslana <pmaslana3@gmail.com>
2024-05-16 14:21:55 -0700

    Add the datastore go file

--- internal/keybase/database.go
+++ internal/keybase/database.go
@@ -4,56 +4,20 @@ import (
 	"fmt"
 )
 
-func (r *Repository) initTables() error {
-	if r.mysqlClient == nil {
+func (d *Datastore) initTables() error {
+	if d.mysqlClient == nil {
 		return fmt.Errorf("mysqlClient not initialized")
 	}
 	query := "CREATE TABLE IF NOT EXISTS `projects` (" +
 		"  `id` bigint unsigned NOT NULL AUTO_INCREMENT," +
 		"  `project` VARCHAR(255) NOT NULL," +
-		"  `ref` VARCHAR(255) NOT NULL," +
-		"  `vars` TEXT NOT NULL," +
+		"  `prnumber` bigint NOT NULL," +
+		"  `msg_last_sent_time` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP," +
 		"  PRIMARY KEY (`id`)," +
-		"  UNIQUE KEY `project-ref-unique` (`project`, `ref`)" +
+		"  UNIQUE KEY `project-pr-number-unique` (`project`, `prnumber`)" +
 		") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;"
 
-	result, err := r.mysqlClient.Query(query)
-	if err != nil {
-		return err
-	}
-	err = result.Close()
-	if err != nil {
-		return err
-	}
-	query = "CREATE TABLE IF NOT EXISTS `workflow_state` (" +
-		"  `id` bigint unsigned NOT NULL AUTO_INCREMENT," +
-		"  `project_id` bigint unsigned NOT NULL ," +
-		"  `workflow_name` VARCHAR(255) NOT NULL," +
-		"  `complete` TINYINT NOT NULL DEFAULT 0," +
-		"  PRIMARY KEY (`id`)," +
-		"  KEY `workflow_state_project_id_foreign` (`project_id`)," +
-		"  UNIQUE KEY `project-workflow-unique` (`project_id`, `workflow_name`)," +
-		"  CONSTRAINT `workflows_state_project_id_foreign` FOREIGN KEY (`project_id`) REFERENCES `projects` (`id`) ON DELETE CASCADE ON UPDATE CASCADE" +
-		") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;"
-
-	result, err = r.mysqlClient.Query(query)
-	if err != nil {
-		return err
-	}
-	err = result.Close()
-	if err != nil {
-		return err
-	}
-
-	query = "CREATE TABLE IF NOT EXISTS `delayed_triggers` (" +
-		"  `id` bigint unsigned NOT NULL AUTO_INCREMENT," +
-		"  `project` VARCHAR(255) NOT NULL," +
-		"  `run_after` bigint unsigned NOT NULL," +
-		"  `vars` TEXT NOT NULL," +
-		"  PRIMARY KEY (`id`)" +
-		") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;"
-
-	result, err = r.mysqlClient.Query(query)
+	result, err := d.mysqlClient.Query(query)
 	if err != nil {
 		return err
 	}
@@ -63,5 +27,4 @@ func (r *Repository) initTables() error {
 	}
 
 	return nil
-}
 }
