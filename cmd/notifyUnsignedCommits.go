369ce16a67f276e7ce197d7c43cd285080c357a8
Patrick Maslana <pmaslana3@gmail.com>
2024-07-03 14:51:49 -0700

    Add unsigned commits bot

---
+++ cmd/notifyUnsignedCommits.go
@@ -0,0 +1,113 @@
+package cmd
+
+import (
+	"context"
+	"time"
+
+	"github.com/google/go-github/v60/github"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+
+	"github.com/chia-network/github-bot/internal/config"
+	"github.com/chia-network/github-bot/internal/database"
+	github2 "github.com/chia-network/github-bot/internal/github"
+	"github.com/chia-network/github-bot/internal/keybase"
+
+	"github.com/chia-network/go-modules/pkg/slogs"
+)
+
+var notifyUnsignedCommitsCmd = &cobra.Command{
+	Use:   "notify-unsigned",
+	Short: "Sends a Keybase message to a channel, alerting of unsigned commits",
+	Run: func(cmd *cobra.Command, args []string) {
+		slogs.Init("info")
+		cfg, err := config.LoadConfig(viper.GetString("config"))
+		if err != nil {
+			slogs.Logr.Fatal("Error loading config", "error", err)
+		}
+		client := github.NewClient(nil).WithAuthToken(cfg.GithubToken)
+
+		datastore, err := database.NewDatastore(
+			viper.GetString("db-host"),
+			viper.GetUint16("db-port"),
+			viper.GetString("db-user"),
+			viper.GetString("db-pass"),
+			viper.GetString("db-name"),
+			"unsignedcommits",
+		)
+
+		if err != nil {
+			slogs.Logr.Error("Could not initialize mysql connection", "error", err)
+			return
+		}
+
+		loop := viper.GetBool("loop")
+		loopDuration := viper.GetDuration("loop-time")
+		ctx := context.Background()
+
+		sendMsgDuration := 24 * time.Hour
+
+		for {
+			slogs.Logr.Info("Checking for community PRs that are waiting for CI to run")
+			listPendingPRs, err := github2.CheckUnsignedCommits(ctx, client, cfg)
+			if err != nil {
+				slogs.Logr.Error("Error obtaining a list of pending PRs", "error", err)
+				time.Sleep(loopDuration)
+				continue
+			}
+
+			for _, pr := range listPendingPRs {
+				prInfo, err := datastore.GetPRData(pr.Repo, int64(pr.PRNumber))
+				if err != nil {
+					slogs.Logr.Error("Error checking PR info in database", "error", err)
+					continue
+				}
+
+				shouldSendMessage := false
+				if prInfo == nil {
+					// New PR, record it and send a message
+					slogs.Logr.Info("Storing data in db")
+					err := datastore.StorePRData(pr.Repo, int64(pr.PRNumber))
+					if err != nil {
+						slogs.Logr.Error("Error storing PR data", "error", err)
+						continue
+					}
+					shouldSendMessage = true
+				} else if time.Since(prInfo.LastMessageSent) > sendMsgDuration {
+					// 24 hours has elapsed since the last message was issued, update the record and send a message
+					slogs.Logr.Info("Updating last_message_sent time in db")
+					err := datastore.StorePRData(pr.Repo, int64(pr.PRNumber))
+					if err != nil {
+						slogs.Logr.Error("Error updating PR data", "error", err)
+						continue
+					}
+					shouldSendMessage = true
+				}
+
+				if shouldSendMessage {
+					status := "firing"
+					title := "The following pull request is either waiting for approval for CI checks to run or has failed checks:"
+					description := pr.URL
+					slogs.Logr.Info("Sending message via keybase")
+					message := keybase.NewMessage(status, title, description)
+					if err := message.SendKeybaseMsg(); err != nil {
+						slogs.Logr.Error("Failed to send message", "error", err)
+						time.Sleep(15 * time.Second) // This is to prevent "error response: 429 Too Many Requests""
+					} else {
+						slogs.Logr.Info("Message sent for PR", "URL", pr.URL)
+					}
+				}
+			}
+
+			if !loop {
+				break
+			}
+			slogs.Logr.Info("Waiting for next iteration", "duration", loopDuration.String())
+			time.Sleep(loopDuration)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(notifyUnsignedCommitsCmd)
+}
---
+++ internal/github/checkUnsigned.go
@@ -0,0 +1,98 @@
+package github
+
+import (
+	"context"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/chia-network/go-modules/pkg/slogs"
+	"github.com/google/go-github/v60/github"
+
+	"github.com/chia-network/github-bot/internal/config"
+)
+
+// UnsignedPR holds information about pending PRs
+type UnsignedPRs struct {
+	Repo     string
+	PRNumber int
+	URL      string
+}
+
+// CheckUnsignedCommits will return a list of PR URLs that have not been updated in the last 7 days by internal team members.
+func CheckUnsignedCommits(ctx context.Context, githubClient *github.Client, cfg *config.Config) ([]UnsignedPRs, error) {
+	var unsignedPRs []UnsignedPRs
+	teamMembers, err := GetTeamMemberList(githubClient, cfg.InternalTeam)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, fullRepo := range cfg.CheckRepos {
+		slogs.Logr.Info("Checking repository", "repository", fullRepo.Name)
+		parts := strings.Split(fullRepo.Name, "/")
+		if len(parts) != 2 {
+			return nil, fmt.Errorf("invalid repository name - must contain owner and repository: %s", fullRepo.Name)
+		}
+		owner, repo := parts[0], parts[1]
+
+		communityPRs, err := FindCommunityPRs(cfg, teamMembers, githubClient, owner, repo, fullRepo.MinimumNumber)
+		if err != nil {
+			return nil, err
+		}
+
+		for _, pr := range communityPRs {
+			repoName := pr.GetBase().GetRepo().GetFullName() // Get the full name of the repository
+			slogs.Logr.Info("Checking if PR has unsigned commits", "PR", pr.GetHTMLURL())
+			unsigned, err := isUnsigned(ctx, githubClient, pr, teamMembers) // Handle both returned values
+			if err != nil {
+				slogs.Logr.Error("Error checking if PR has unsigned commits", "repository", repoName, "error", err)
+				continue // Skip this PR or handle the error appropriately
+			}
+			if unsigned {
+				slogs.Logr.Info("PR has unsigned commits", "PR", pr.GetNumber(), "repository", fullRepo.Name, "user", pr.User.GetLogin(), "created_at", pr.CreatedAt)
+				unsignedPRs = append(unsignedPRs, UnsignedPRs{
+					Repo:     repo,
+					PRNumber: pr.GetNumber(),
+					URL:      pr.GetHTMLURL(),
+				})
+			} else {
+				slogs.Logr.Info("No commits are unsigned",
+					"PR", pr.GetNumber(),
+					"repository", fullRepo.Name)
+			}
+		}
+	}
+
+	return unsignedPRs, nil
+}
+
+// Checks if a PR has unsigned commits based on the last update from team members
+func isUnsigned(ctx context.Context, githubClient *github.Client, pr *github.PullRequest, teamMembers map[string]bool) (bool, error) {
+	listOptions := &github.ListOptions{PerPage: 100}
+	for {
+		// Create a context for each request
+		unsignedCtx, unsignedCancel := context.WithTimeout(ctx, 30*time.Second) // 30 seconds timeout for each request
+		defer unsignedCancel()
+		commits, resp, err := githubClient.PullRequests.ListCommits(unsignedCtx, pr.Base.Repo.Owner.GetLogin(), pr.Base.Repo.GetName(), pr.GetNumber(), listOptions)
+		if err != nil {
+			slogs.Logr.Error("Failed to get commits for PR", "PR", pr.GetNumber(), "repository", pr.Base.Repo.GetName(), "error", err)
+			return false, err
+		}
+		// Check each commit to see if it is signed
+		for _, commit := range commits {
+			if commit == nil || commit.Commit == nil {
+				continue
+			}
+			verification := commit.Commit.Verification
+			if verification == nil || *verification.Verified == false {
+				return true, nil
+			}
+		}
+		unsignedCancel() // Clean up the context at the end of the loop iteration
+		if resp.NextPage == 0 {
+			break
+		}
+		listOptions.Page = resp.NextPage
+	}
+	return true, nil
+}
